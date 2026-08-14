e6238897fb0e7e4c0dea1a1f20fc4ef4005e2e77
OMAR <OMAR@comets.com>
2026-08-14 21:42:32 +0900

    feat: create  components related modules

---
+++ apps/nestar-api/src/components/components.module.ts
@@ -0,0 +1,28 @@
+
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { BoardArticleModule } from './board-article/board-article.module';
+import { MemberModule } from './member/member.module';
+import { PropertyModule } from './property/property.module';
+import { Module } from '@nestjs/common';
+
+@Module({
+    imports: [
+        MemberModule,
+        AuthModule,
+        PropertyModule,
+        BoardArticleModule,
+        LikeModule,
+        ViewModule,
+        CommentModule,
+        FollowModule
+
+
+        
+    ],
+})
+
+export class ComponentModule{}
