b6003e56e08ff9df81b797012194da17825ffc9c
OMAR <OMAR@comets.com>
2026-08-17 23:18:34 +0900

    feat: create member resolver and service model

---
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -0,0 +1,38 @@
+import { Mutation, Resolver , Query } from '@nestjs/graphql';
+import { MemberService } from './member.service';
+
+
+@Resolver()
+export class MemberResolver {
+    constructor(private readonly memberService: MemberService){
+
+    }
+
+    @Mutation(() => String)
+    public async signup():Promise<string>{
+        console.log("signup")
+            return this.memberService.signup()
+        
+    }
+
+      @Mutation(() => String)
+    public async login():Promise<string>{
+        console.log("login")
+            return this.memberService.login()
+        
+    }
+
+      @Mutation(() => String)
+    public async updateMember():Promise<string>{
+        console.log("updateMember")
+            return this.memberService.updateMember()
+        
+    }
+
+      @Query(() => String)
+    public async getMember():Promise<string>{
+        console.log("getMember")
+            return this.memberService.getMember()
+        
+    }
+}
