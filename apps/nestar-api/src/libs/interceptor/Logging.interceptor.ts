9bdc36b684e9950b3e3079337676443777bfcf5f
OMAR <OMAR@comets.com>
2026-08-18 23:13:27 +0900

    feat: develop logging via interceptors

--- apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
+++ apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
@@ -1,26 +1,42 @@
-import {
-  CallHandler,
-  ExecutionContext,
-  Injectable,
-  NestInterceptor,
-} from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  intercept(
-    context: ExecutionContext,
-    next: CallHandler,
-  ): Observable<any> {
-    console.log('Before...');
+	private readonly logger: Logger = new Logger(); //Kiruvchi va chiquvchi malumotlarni terminalda korinib turadigan qilamiz
 
-    const now = Date.now();
+	public intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+		const recordTime = Date.now();
+		const requestType = context.getType<GqlContextType>(); //response HTTP yoki GraphQl ekanligini tekshiramiz
 
-    return next
-      .handle()
-      .pipe(
-        tap(() => console.log(`After... ${Date.now() - now}ms`)),
-      );
-  }
+		if (requestType === 'http') {
+			// Develop if needed!
+		} else if (requestType === 'graphql') {
+			/** (1) Print Request */
+			const gqlContext = GqlExecutionContext.create(context);
+			this.logger.log(`${this.stringify(gqlContext.getContext().req.body)}`, 'REQUEST');
+
+			/** (2) Errors handing via GraphQL */
+
+			/** (3) No Errors, giving Response below */
+			return next.handle().pipe(
+				tap((context) => {
+					//bu yerdagi context responsedan kelayotgan context
+					const responseTime = Date.now() - recordTime;
+					this.logger.log(`${this.stringify(context)} - ${responseTime}ms \n\n`, 'RESPONSE');
+				}), //res vaqti va datasi chop etiladi
+			);
+		}
+
+		return next.handle(); // boshqa holatlar uchun ham qaytarish
+	}
+
+	private stringify(context: ExecutionContext): string {
+		return JSON.stringify(context).slice(0, 75); //req.bodyni objectdan jsonga ogirib oldik.0 dan 75 harfgacha kesib oldik
+	}
 }
+
+//RestAPI request = HTTP REQUEST
+//GraphQL request = GraphQl REQUEST
