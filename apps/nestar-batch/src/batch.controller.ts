52a75b1ce7d0a14fe83885450ed33b6092698eb1
OMAR <OMAR@comets.com>
2026-09-06 18:45:36 +0900

    faet: create batchRollBack , batchTopProperties and batchTopAgents schedule

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,12 +1,61 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './lib/config';
 
 @Controller()
 export class BatchController {
-  constructor(private readonly BatchService: BatchService) {}
+   
+   private logger: Logger = new Logger('batchController');
+  constructor(private readonly batchService: BatchService) {}
+
+
+
+@Timeout(3000)
+handleTimeOut(){
+  this.logger.debug('BATCH SERVER READY')
+}
+
+@Cron('40 * * * * *', {name: BATCH_ROLLBACK})
+public async batchRollBack(){
+  try{
+   this.logger['context'] =BATCH_ROLLBACK
+   this.logger.debug("EXECUTED")
+   await this.batchService.batchRollBack()
+}catch(err){
+  this.logger.error(err)
+}}
+
+@Cron('45 * * * * *', {name: BATCH_TOP_PROPERTIES})
+public async batchTopProperties(){
+  try{
+   this.logger['context'] = BATCH_TOP_PROPERTIES
+   this.logger.debug("EXECUTED")
+   await this.batchService.batchTopProperties()
+}catch(err){
+  this.logger.error(err)
+}}
+
+
+@Cron('50 * * * * *', {name: BATCH_TOP_AGENTS})
+public async batchTopAgents(){
+  try{
+   this.logger['context'] =BATCH_TOP_AGENTS
+   this.logger.debug("EXECUTED")
+   await this.batchService.batchTopAgents()
+}catch(err){
+  this.logger.error(err)
+}}
+/*
+@Interval(1000)
+  handleInterval(){
+  this.logger.debug("** Interval server **")
+}
+  */
+
 
   @Get()
   getHello(): string {
-    return this.BatchService.getHello();
+    return this.batchService.getHello();
   }
 }
--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -5,4 +5,16 @@ export class BatchService {
   getHello(): string {
     return 'Nestar Batch is running!';
   }
+
+  public async batchRollBack(): Promise<void>{
+    console.log("batchRollBack")
+  }
+
+    public async batchTopProperties(): Promise<void>{
+    console.log("batchTopProperties")
+  }
+
+    public async batchTopAgents(): Promise<void>{
+    console.log("batchTopAgents")
+  }
 }
